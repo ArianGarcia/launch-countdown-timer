3d812c65e500509ab15179adfe72724a61100c63
Arian Garcia <arm.garcia@motorcredito.com.do>
2022-05-24 19:33:43 -0400

    clock countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,12 +1,34 @@
 import CountdownNumberCard from './CountdownNumberCard';
 
 const Countdown = () => {
+  
+  const calculateTimeLeft = (type: any) => {
+    const times = {
+      'DAYS': calculateDaysLeft,
+      'HOURS': calculateDaysLeft,
+      'MINUTES': calculateDaysLeft,
+      'SECONDS': calculateDaysLeft,
+    }
+    console.log(times[type]());
+    
+    return (12).toString();
+  }
+
+  const calculateDaysLeft = () => {
+    return 23
+  }
+
+  // TODO: change array to a object literal
   const clockData = [
-    {text: 'DAYS', count: '08'},
-    {text: 'HOURS', count: '23'},
-    {text: 'MINUTES', count: '55'},
-    {text: 'SECONDS', count: '41'},
+    {text: 'DAYS', count: calculateTimeLeft('DAYS')},
+    {text: 'HOURS', count: calculateTimeLeft('HOURS')},
+    {text: 'MINUTES', count: calculateTimeLeft('MINUTES')},
+    {text: 'SECONDS', count: calculateTimeLeft('SECONDS')},
   ];
+
+  /* TODO: Calculate date with a while by incrementing
+  in 22 days every time the actual date pass the due date
+  */
 
   const clockCards = clockData.map((item) => {
     return (
