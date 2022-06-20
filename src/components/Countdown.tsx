f7ef5e37bc9ab5296aa1af4e77c284d56658b142
Arian Garcia <arm.garcia@motorcredito.com.do>
2022-06-20 11:12:02 -0400

    Add the countdown funcionality to the clock

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,48 +1,15 @@
 import CountdownNumberCard from './CountdownNumberCard';
+import { useCountdown } from '../hooks/useCountdown';
 
-const Countdown = () => {
-  
-  const calculateTimeLeft = (type: any) => {
-    const times = {
-      'DAYS': calculateDaysLeft,
-      'HOURS': calculateDaysLeft,
-      'MINUTES': calculateDaysLeft,
-      'SECONDS': calculateDaysLeft,
-    }
-    console.log(times[type]());
-    
-    return (12).toString();
-  }
-
-  const calculateDaysLeft = () => {
-    return 23
-  }
-
-  // TODO: change array to a object literal
-  const clockData = [
-    {text: 'DAYS', count: calculateTimeLeft('DAYS')},
-    {text: 'HOURS', count: calculateTimeLeft('HOURS')},
-    {text: 'MINUTES', count: calculateTimeLeft('MINUTES')},
-    {text: 'SECONDS', count: calculateTimeLeft('SECONDS')},
-  ];
-
-  /* TODO: Calculate date with a while by incrementing
-  in 22 days every time the actual date pass the due date
-  */
-
-  const clockCards = clockData.map((item) => {
-    return (
-      <CountdownNumberCard
-        key={`${item.text}_Card`}
-        text={item.text}
-        count={item.count}
-      />
-    );
-  });
+const Countdown = ({dueDate}: {dueDate: number}) => {
+  const [days, hours, minutes, seconds] = useCountdown(dueDate);
 
   return (
     <div id='timer'>
-      {clockCards}
+      <CountdownNumberCard text="DAYS" count={days.toString()} />
+      <CountdownNumberCard text="HOURS" count={hours.toString()} />
+      <CountdownNumberCard text="MINUTES" count={minutes.toString()} />
+      <CountdownNumberCard text="SECONDS" count={seconds.toString()} />
     </div>
   )
 }
